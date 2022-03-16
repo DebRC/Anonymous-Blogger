50072a84dcb5e97c489756cefae3ea993bda331a
Debdoot Roy Chowdhury <roychowdhurydebdoot@gmail.com>
2022-03-16 19:37:23 +0530

    Re:Moved tile.dart to Lib

--- lib/home.dart
+++ lib/home.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/foundation.dart';
-import 'package:blogapp/blogstyle/tile.dart';
+import 'package:blogapp/tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
